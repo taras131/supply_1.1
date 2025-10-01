e6d6c82b8fa2919915502f0f63c1e2ecab0c10f0
taras131 <mossnabitkana@gmail.com>
2025-10-01 23:26:36 +0300

    added change password component_2

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const nestServerPath = "http://77.110.104.31";
+export const nestServerPath = "";
 
 export async function getNewAccessToken() {
   const response = await fetch(`${nestServerPath}/auth/refresh`, {
